f1a12c91a633160fb9e717b4d6572b4346108c0d
kgarchie <archiethebig@gmail.com>
2024-03-31 21:30:10 +0300

    Run tweak

--- server/utils/vercel.ts
+++ server/utils/vercel.ts
@@ -2,4 +2,5 @@ function checkVercel() {
     return !!(process.env.VERCEL || process.env.NOW_REGION);
 }
 
+/** Detect whether the app is running on Vercel */
 export const isVercel = checkVercel();
