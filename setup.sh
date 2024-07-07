0b9ad76a6d37ff53853c3b8b4db5f22798d5b562
kgarchie <archiethebig@gmail.com>
2024-07-07 09:36:57 +0300

    edited setup

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@ if [[ "$OSTYPE" != "linux-gnu"* ]]; then
     exit 1
 fi
 
+if [[ $NODE_ENV == "production" || $NODE_ENV == "prod" ]]; then
+    echo "This script is for development only"
+    exit 0
+fi
+
 if [ -n "$VERCEL" ] || [ -n "$NOW_REGION" ]; then
     echo "This script will not work on a serverless environment"
     exit 0
@@ -53,8 +58,3 @@ function add_on() {
   #sudo apt install -y gconf-service libgbm-dev libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget -y
   echo "Additional dependencies are disabled for now"
 }
-
-pre_check
-prerequisites
-pull
-install
