4f2df6ee846165de66e23244ed8582841c72e495
Kemboi Elvis <85908528+kemboi22@users.noreply.github.com>
2024-07-17 02:09:25 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,9 @@
                 Your success story starts with us.
               </h1>
               <p class="mt-4 text-lg">
-                See how you can automate payments and increase your revenue
+                Supply Chain end to end efficiency 
+                Get started with
+                Automated Marketing/Sales, Purchase/Orders, Payments till Receipt/Order Fulfillment😋Receipt/Order
               </p>
               <button @click="navigateTo(userIsAuthenticated() ? '/forms' : '/login?redirect=/forms')"
                 class="bg-white text-gray-800 active:bg-gray-100 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 mt-3 hover:bg-slate-300 transition-all duration-500 ease-in-out">Get
