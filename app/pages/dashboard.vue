9613acd23d000a5b9cbbbe0d5a0a7fb924362629
kgarchie <archiethebig@gmail.com>
2024-12-04 15:08:54 +0300

    Conditional rendering on the card bars

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -83,7 +83,7 @@ const recents = await useFetch<APIResponse<{ forms: Drizzle.Form.select[] }>>(
         :description="`You have created ${stats?.forms || 0} forms so far`"
         style="background: radial-gradient(100.76% 179.14% at -2.4% -2.78%, #F3F3F3 25.3%, #E0FBFC 100%)"
         class="p-6 ring-1 ring-sky rounded shadow" />
-      <div class="h-1 w-full bg-transparent rounded -mt-1">
+      <div class="h-1 w-full bg-transparent rounded -mt-1" v-if="stats.forms < 10">
         <div class="h-1 bg-sky rounded max-w-full" :style="{ width: `${((stats?.forms || 0) / 10) * 100}%` }" />
       </div>
     </NuxtLink>
@@ -92,8 +92,8 @@ const recents = await useFetch<APIResponse<{ forms: Drizzle.Form.select[] }>>(
       <Card title="money" :icon="DollarSignIcon" :count="stats.earnings"
         :description="`You have earned ${stats?.earnings || 0} KES so far`" class="p-6 ring-1 ring-sky rounded shadow"
         style="background: radial-gradient(100.76% 179.14% at -2.4% -2.78%, #F3F3F3 25.3%, #E0FBFC 100%)" />
-      <div class="h-1 w-full bg-transparent rounded -mt-1">
-        <div class="h-1 bg-sky rounded max-w-full" :style="{ width: `${((stats?.earnings || 0) / 10) * 100}%` }" />
+      <div class="h-1 w-full bg-transparent rounded -mt-1" v-if="stats.earnings < 10000">
+        <div class="h-1 bg-sky rounded max-w-full" :style="{ width: `${((stats?.earnings || 0) / 10000) * 100}%` }" />
       </div>
     </NuxtLink>
     <NuxtLink to="/marketplace"
@@ -102,7 +102,7 @@ const recents = await useFetch<APIResponse<{ forms: Drizzle.Form.select[] }>>(
         :description="`You have reached ${stats?.responses || 0} people`"
         class="p-6 ring-1 ring-sky rounded shadow"
         style="background: radial-gradient(100.76% 179.14% at -2.4% -2.78%, #F3F3F3 25.3%, #E0FBFC 100%)" />
-      <div class="h-1 w-full bg-transparent rounded -mt-1">
+      <div class="h-1 w-full bg-transparent rounded -mt-1" v-if="stats.responses < 10">
         <div class="h-1 bg-sky rounded max-w-full" :style="{ width: `${((stats?.responses || 0) / 10) * 100}%` }" />
       </div>
     </NuxtLink>
