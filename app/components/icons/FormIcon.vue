f516dce96986218ef629ea79c8c5988bf9830b3d
Kemboi Elvis <kemboielvis22@gmail.com>
2025-05-14 18:24:53 +0300

    feat: extracted icons from index.vue

---
+++ app/components/icons/FormIcon.vue
@@ -0,0 +1,38 @@
+<template>
+	<svg
+		data-v-149cb24b=""
+		width="140"
+		height="140"
+		viewBox="0 0 140 140"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			data-v-149cb24b=""
+			d="M23.3333 40.8333C23.3333 29.8339 23.3333 24.3342 26.7504 20.9171C30.1674 17.5 35.6671 17.5 46.6666 17.5H93.3333C104.333 17.5 109.832 17.5 113.249 20.9171C116.667 24.3342 116.667 29.8339 116.667 40.8333V87.5C116.667 103.999 116.667 112.249 111.541 117.374C106.415 122.5 98.1656 122.5 81.6666 122.5H58.3333C41.8341 122.5 33.5845 122.5 28.4589 117.374C23.3333 112.249 23.3333 103.999 23.3333 87.5V40.8333Z"
+			stroke="#3D5A80"
+			stroke-width="11.6667"
+		></path>
+		<path
+			data-v-149cb24b=""
+			d="M87.5 105V122.5M52.5 105V122.5"
+			stroke="#3D5A80"
+			stroke-width="11.6667"
+			stroke-linecap="round"
+		></path>
+		<path
+			data-v-149cb24b=""
+			d="M52.5 46.6666H87.5"
+			stroke="#3D5A80"
+			stroke-width="11.6667"
+			stroke-linecap="round"
+		></path>
+		<path
+			data-v-149cb24b=""
+			d="M52.5 70H87.5"
+			stroke="#3D5A80"
+			stroke-width="11.6667"
+			stroke-linecap="round"
+		></path>
+	</svg>
+</template>
