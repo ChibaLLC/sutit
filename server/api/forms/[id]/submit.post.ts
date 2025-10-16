4347d9a6fd795ce7f92753643d13aec9b76a177d
Kemboi Elvis <kemboielvis22@gmail.com>
2025-10-16 05:06:36 +0300

    fix: back to black and white and also removed after message

--- server/api/forms/[id]/submit.post.ts
+++ server/api/forms/[id]/submit.post.ts
@@ -3,52 +3,43 @@ import { processFormPayment } from "~~/server/services/payment.service";
 import { submitForm } from "~~/server/services/submissions.service";
 
 export default defineEventHandler(async (event) => {
-	try {
-		const formId = getRouterParam(event, "id");
-		if (!formId) {
-			throw createError({
-				message: "Not Found",
-				statusCode: 404,
-			});
-		}
-		const session = await auth.api.getSession({
-			headers: event.headers,
-		});
-		const body = await readBody(event);
-		let submission = await submitForm(formId, body, session?.user.id);
-		if (submission.submmission.pricePaid == 0) {
-			return {
-				data: {
-					...submission,
-				},
-				message: "submitted successfully",
-			};
-		}
-		const pay = await processFormPayment(
-			submission.form,
-			submission.submmission,
-		);
-		if (
-			body.paymentData.phoneNumber &&
-			submission.form.afterSubmissionMessage
-		) {
-			await sendTextSmsTiara({
-				phone: body.paymentData.phoneNumber as string,
-				message: submission.form.afterSubmissionMessage,
-			});
-		}
+  try {
+    const formId = getRouterParam(event, "id");
+    if (!formId) {
+      throw createError({
+        message: "Not Found",
+        statusCode: 404,
+      });
+    }
+    const session = await auth.api.getSession({
+      headers: event.headers,
+    });
+    const body = await readBody(event);
+    let submission = await submitForm(formId, body, session?.user.id);
+    if (submission.submmission.pricePaid == 0) {
+      return {
+        data: {
+          ...submission,
+        },
+        message: "submitted successfully",
+      };
+    }
+    const pay = await processFormPayment(
+      submission.form,
+      submission.submmission,
+    );
 
-		return {
-			data: {
-				...submission,
-				...pay,
-			},
-			message: "Stk Push Has been sent to your phone Pay",
-		};
-	} catch (e) {
-		throw createError({
-			statusCode: 500,
-			message: e.message || "An error occurred!!",
-		});
-	}
+    return {
+      data: {
+        ...submission,
+        ...pay,
+      },
+      message: "Stk Push Has been sent to your phone Pay",
+    };
+  } catch (e) {
+    throw createError({
+      statusCode: 500,
+      message: e.message || "An error occurred!!",
+    });
+  }
 });
